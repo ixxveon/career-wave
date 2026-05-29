c2cac76a8e4552d4900cceba3dbe1adcefd5ae3b
sbr7518 <sbr7518@gmail.com>
2026-05-29 15:19:02 +0900

    chore: remove unused admin pages and orphaned api file
    - remove admin/JobNotice page (moved to user side)
    - remove admin/Matching page (replaced by Statistics)
    - remove settlementApi.js (not referenced anywhere)

--- frontend/src/admin/api/settlementApi.js
+++
@@ -1,7 +0,0 @@
-import axiosInstance from '../../utils/axiosInstance';
-
-export const settlementApi = {
-  getList: (params) => axiosInstance.get('/api/admin/settlements', { params }),
-  getDetail: (id) => axiosInstance.get(`/api/admin/settlements/${id}`),
-  updateStatus: (id, data) => axiosInstance.patch(`/api/admin/settlements/${id}/status`, data),
-};
