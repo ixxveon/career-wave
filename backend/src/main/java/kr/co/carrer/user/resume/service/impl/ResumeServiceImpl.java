26721f1cd48720c355068a0b49eb2e998f08b7e9
ixxveon <igayeon322s@gmail.com>
2026-06-11 00:32:20 +0900

    refactor(feature/backend-user-resume-upload) : ResumeServiceImpl service/impl/ 서브패키지로 이동

--- backend/src/main/java/kr/co/carrer/user/resume/service/impl/ResumeServiceImpl.java
+++ backend/src/main/java/kr/co/carrer/user/resume/service/impl/ResumeServiceImpl.java
@@ -1,4 +1,4 @@
-package kr.co.carrer.user.resume.service;
+package kr.co.carrer.user.resume.service.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
