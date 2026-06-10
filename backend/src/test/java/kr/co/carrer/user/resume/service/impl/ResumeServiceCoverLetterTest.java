1ea659ed8c02967fe1cdfae79af37331ab5f2d21
ixxveon <igayeon322s@gmail.com>
2026-06-11 01:03:32 +0900

    fix(feature/backend-user-resume-history) : 테스트 파일 service 패키지 import 누락 추가

--- backend/src/test/java/kr/co/carrer/user/resume/service/impl/ResumeServiceCoverLetterTest.java
+++ backend/src/test/java/kr/co/carrer/user/resume/service/impl/ResumeServiceCoverLetterTest.java
@@ -10,6 +10,8 @@ import kr.co.carrer.user.resume.entity.Document;
 import kr.co.carrer.user.resume.repository.CoverLetterContentRepository;
 import kr.co.carrer.user.resume.repository.CoverLetterMetaRepository;
 import kr.co.carrer.user.resume.repository.DocumentRepository;
+import kr.co.carrer.user.resume.service.FastApiClient;
+import kr.co.carrer.user.resume.service.FileValidator;
 import kr.co.carrer.user.resume.type.FileType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
