9f8869caf5ee12085f225352b884dc2cd1ebbb41
hongsoonchan02 <asm640789@gmail.com>
2026-06-15 16:45:10 +0900

    fix(fix/admin-repository-bean-conflict) : 관리자 repository bean 충돌 수정

--- backend/src/main/java/kr/co/carrer/admin/admin/repository/AdminRepository.java
+++ backend/src/main/java/kr/co/carrer/admin/admin/repository/AdminRepository.java
@@ -2,7 +2,9 @@ package kr.co.carrer.admin.admin.repository;
 
 import kr.co.carrer.admin.admin.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("adminManagementRepository")
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
     boolean existsByEmail(String email);
