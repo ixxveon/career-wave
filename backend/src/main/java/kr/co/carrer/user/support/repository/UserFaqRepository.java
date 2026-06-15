a34a102b1007d8daca4c1cc35daec50fdc3156d0
Bora <sbr7518@gmail.com>
2026-06-15 20:23:19 +0900

    remove: 미사용 UserFaqRepository 삭제
    FAQ는 READ 전용으로 JPA 기본 메서드 사용 불필요
    쿼리 로직은 UserFaqQueryRepository(Native Query)에서 처리

--- backend/src/main/java/kr/co/carrer/user/support/repository/UserFaqRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.carrer.user.support.repository;
-
-import kr.co.carrer.user.support.entity.SupportFaq;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserFaqRepository extends JpaRepository<SupportFaq, Long> {
-}
