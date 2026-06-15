20cecd9d8e82b2d6dc013d4e91ce528304a35d94
hongsoonchan02 <asm640789@gmail.com>
2026-06-15 14:23:38 +0900

    fix(feature/backend-user-jobnotice-test) : narrow repository test entity scan

--- backend/src/test/java/kr/co/carrer/user/jobnotice/repository/JobNoticeQueryRepositoryTest.java
+++ backend/src/test/java/kr/co/carrer/user/jobnotice/repository/JobNoticeQueryRepositoryTest.java
@@ -10,6 +10,7 @@ import kr.co.carrer.user.jobnotice.type.JobType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
@@ -28,6 +29,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 @Import(JobNoticeQueryRepository.class)
+@EntityScan(basePackageClasses = JobNotice.class)
 @TestPropertySource(properties = {
         "spring.sql.init.mode=never",
         "spring.main.allow-bean-definition-overriding=true"
