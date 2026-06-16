9703da0058f058a1e181169c6e9c4a7548c5f793
hongsoonchan02 <asm640789@gmail.com>
2026-06-16 12:09:35 +0900

    fix(backend-admin-management-docs) : Swagger 보안 어노테이션 import 추가

--- backend/src/main/java/kr/co/carrer/admin/admin/docs/AdminManagementDocs.java
+++ backend/src/main/java/kr/co/carrer/admin/admin/docs/AdminManagementDocs.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
