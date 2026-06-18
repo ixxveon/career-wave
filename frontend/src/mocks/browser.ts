c78f87ebd20e8ae3bcb2f1bb1108f7095888d13f
ixxveon <igayeon322s@gmail.com>
2026-06-19 01:28:47 +0900

    chore(user-resume-integration) : remove resume MSW mocks (real backend integration)

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -1,14 +1,12 @@
 import { setupWorker } from 'msw/browser';
 import { memberHandlers } from './user/memberHandlers';
 import { subscriptionHandlers } from './user/subscriptionHandlers';
-import { resumeHandlers } from './user/resumeHandlers';
 import { jobNoticeHandlers } from './user/jobNoticeHandlers';
 import { adminHandlers } from './admin/handlers';
 
 export const worker = setupWorker(
   ...memberHandlers,
   ...subscriptionHandlers,
-  ...resumeHandlers,
   ...jobNoticeHandlers,
   ...adminHandlers,
 );
