8a391c6d403d86b544519b2a3593568c658102e4
hongsoonchan02 <asm640789@gmail.com>
2026-06-23 17:49:20 +0900

    fix(fix/admin-dashboard-service-bean-name) : 관리자 대시보드 서비스 빈 이름 충돌 해결

--- backend/src/main/java/kr/co/carrer/admin/dashboard/service/impl/DashboardServiceImpl.java
+++ backend/src/main/java/kr/co/carrer/admin/dashboard/service/impl/DashboardServiceImpl.java
@@ -24,7 +24,7 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+@Service("adminDashboardServiceImpl")
 @RequiredArgsConstructor
 public class DashboardServiceImpl implements DashboardService {
 
