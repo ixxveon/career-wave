b662cb840275b87b7328f3a3d250457178d878f9
hongsoonchan02 <asm640789@gmail.com>
2026-06-30 09:52:40 +0900

    fix(pr-906-backend-fix) : Mockito mock 정적 import 추가

--- backend/src/test/java/kr/co/carrer/admin/dashboard/service/impl/DashboardServiceImplTest.java
+++ backend/src/test/java/kr/co/carrer/admin/dashboard/service/impl/DashboardServiceImplTest.java
@@ -26,6 +26,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
