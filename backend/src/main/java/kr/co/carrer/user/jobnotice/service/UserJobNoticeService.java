9c66586c460f7062a5a656e55f8b6e889867a2c1
hongsoonchan02 <asm640789@gmail.com>
2026-07-20 00:48:44 +0900

    chore(1365-unused-dependency-cleanup): 미사용 의존성 정리

--- backend/src/main/java/kr/co/carrer/user/jobnotice/service/UserJobNoticeService.java
+++ backend/src/main/java/kr/co/carrer/user/jobnotice/service/UserJobNoticeService.java
@@ -1,7 +1,6 @@
 package kr.co.carrer.user.jobnotice.service;
 
 import kr.co.carrer.user.jobnotice.dto.JobNoticeDTO;
-import kr.co.carrer.user.jobnotice.type.JobNoticeStatus;
 import kr.co.carrer.user.jobnotice.type.CareerLevel;
 import kr.co.carrer.user.jobnotice.type.CompanySize;
 import kr.co.carrer.user.jobnotice.type.JobType;
